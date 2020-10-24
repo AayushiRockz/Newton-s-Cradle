a517b15320089d6b2a45b5123baf21e5c5b4715a
AayushiRockz <71376337+AayushiRockz@users.noreply.github.com>
2020-10-24 16:44:34 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,33 @@
+class rope{
+	constructor(body1,body2, offsetX, offsetY)
+	{
+		this.offsetX=offsetX
+		this.offsetY=offsetY
+		var options={
+			bodyA:body1,
+			bodyB:body2,
+			pointB:{x:this.offsetX, y:this.offsetY}
+		}
+		//console.log(options);
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+	}
+
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+
+		strokeWeight(3);
+		stroke("cyan");
+
+		var Ax=pointA.x
+		var Ay=pointA.y
+
+		var Bx=pointB.x+this.offsetX
+		var By=pointB.y+this.offsetY
+
+		line(Ax,Ay,Bx,By);
+	}
+
+}
